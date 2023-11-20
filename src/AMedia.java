be6af73209efc92ce5f817e2d32e7db264de1178
sanderMarcusChristensen <144114615+sanderMarcusChristensen@users.noreply.github.com>
2023-11-20 10:38:53 +0100

    Media

---
+++ src/AMedia.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public abstract class AMedia {
+    private String name;
+    private float rating;
+    private int releasYear;
+    private ArrayList<String> categories;
+
+
+
+
+    @Override
+    public String toString()
+    {
+        return null;
+    }
+}
---
+++ src/Series.java
@@ -0,0 +1,26 @@
+import java.util.Map;
+
+public class Series {
+
+    private int endYear;
+    private Map<Integer,Integer> seasons;
+
+
+    public Map<Integer,Integer> getSeasons()
+    {
+        return seasons;
+    }
+
+    public int getEndYears()
+    {
+        return endYear;
+    }
+
+    @Override
+    public String toString()
+    {
+        return null;
+    }
+
+
+}
