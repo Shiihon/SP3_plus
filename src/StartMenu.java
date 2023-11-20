23da830854c58dfe86b8b86e9b0ecf6f902c9719
Shiihon <144337411+Shiihon@users.noreply.github.com>
2023-11-20 12:11:31 +0100

    Update branch Nadia

--- src/StartMenu.java
+++ src/StartMenu.java
@@ -1,38 +1,53 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class StartMenu  extends AMenu {
+public class StartMenu extends AMenu {
 
-    private final Map<String, String > users;
+    private IO io;
+
+    private final Map<String, String> users;
 
     public StartMenu() {
+
         users = new HashMap<>();
+        io = new FileIO();
     }
 
-    private boolean login(String userName, String password){
+    private boolean login(String userName, String password) {
         return false;
     }
-    private boolean register(String userName, String password){
-       return false;
+
+    private boolean register(String userName, String password) {
+        return false;
     }
 
-    private void runUserDialog(){
+    private void runUserDialog() {
 
     }
 
-    private void loadUserData(String userName){
+    private void loadUserData(String userName) {
 
     }
+
     @Override
     public void setup() {
 
     }
 
-    public void loadUsers(){
-
+    public void loadUsers() {
+        ArrayList<String> userDataList = io.readData("users.txt");
+        users.clear();
+        for (String line : userDataList) {
+            String[] userData = line.split(",");
+            String userName = userData[0].trim();
+            String password = userData[1].trim();
+            users.put(userName, password);
+        }
     }
 
-    public void saveUsers(){
+    public void saveUsers(ArrayList<User> registeredUsers) {
 
     }
 
