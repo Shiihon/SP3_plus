561ab1e4020f02e7853c3c4d47b06d42897fae25
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2023-11-23 10:52:38 +0100

    Fix endYear out of bounds

--- src/StartMenu.java
+++ src/StartMenu.java
@@ -126,7 +126,11 @@ public class StartMenu extends AMenu {
             String runningYears = mediaData[2].trim();
             String[] runningYearData = runningYears.split("-");
             int releaseYear = Integer.parseInt(runningYearData[0].trim());
-            int endYear = Integer.parseInt(runningYearData[1].trim());
+            int endYear = -1;
+
+            if (runningYearData.length > 1) {
+                endYear = Integer.parseInt(runningYearData[1].trim());
+            }
 
             String seasons = mediaData[5].trim();
             String[] numberOfSeasons = seasons.split(",");
